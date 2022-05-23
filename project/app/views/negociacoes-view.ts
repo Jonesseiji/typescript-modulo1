3a004db9ff6d02aa9d3da1d6847cb07e5c3bc81d
Jones Seiji <jonesseiji@gmail.com>
2022-05-23 09:31:37 -0300

    integrate view to page

--- project/app/views/negociacoes-view.ts
+++ project/app/views/negociacoes-view.ts
@@ -1,17 +1,28 @@
 
 export class NegociacoesView {
+    private elemento: HTMLElement;
+
+    constructor(selector: string) {
+        this.elemento = document.querySelector(selector);
+    }
 
     template(): string {
         return `
-        <table class="table table-hover table-bordered">
-            <thead>
-            <tr>
-                <th> Data </th>
-                <th> Quantidade </th>
-                <th> Valor </th>
-            </tr>
-            </thead>
-        </table>
-        `
+            <table class="table table-hover table-bordered" >
+                <thead>
+                    <tr>
+                        <th> Data </th>
+                        <th> Quantidade </th>
+                        <th> Valor </th>
+                    </tr>
+                </thead>
+                <tbody>
+                </tbody>
+            </table>
+        `;
+    }
+
+    update(): void {
+        this.elemento.innerHTML = this.template();
     }
 }
